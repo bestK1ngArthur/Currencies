400508961a1b3a52818a9ef687dbe7ab8642c75b
Artem Belkov <bestK1ngArthur@aol.com>
2017-09-14 15:27:11 +0300

    Initial preparation of the project

--- Currencies/CurrenciesController.swift
+++ Currencies/CurrenciesController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  CurrenciesController.swift
 //  Currencies
 //
 //  Created by Artem Belkov on 14/09/2017.
@@ -8,18 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class CurrenciesController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
